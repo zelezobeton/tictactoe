b3456d3000701c719ee609460d62914d53152465
Josef Adamek <pepe.adamek@gmail.com>
2025-07-26 11:25:57 +0200

    Working V5 (mnk game with pygame frontend and runs on web)

---
+++ backend.py
@@ -0,0 +1,301 @@
+import random
+import re
+import time
+
+def replace_char_at_index(string, index, char):
+    return string[:index] + char + string[index + 1:]
+
+def create_field_template(n):
+    s1 = "+---+"
+    s2 = "|   |"
+    s3 = "---+"
+    s4 = "   |"
+    x = n + 1
+    field_list = []
+    m = (x * 3) - (x - 1) 
+    for a in range(m):
+        if a % 2 == 0:
+            row = []
+            for b in range(x):
+                if b == 0:
+                    row.append(s1)
+                else:
+                    row.append(s3)
+        else:
+            row = []
+            for b in range(x):
+                if b == 0 and a == 1:
+                    row.append(s2)
+                elif b == 0 and a > 1:
+                    row.append(f"|{(a - 1) // 2:{" "}>3}|")
+                else:
+                    if a == 1:
+                        row.append(f"{b:{" "}>3}|")
+                    else:
+                        row.append(s4)
+        field_list.append("".join(row))
+    return "\n".join(field_list)
+
+def put_char_on_field(field, char, indexes):
+    START_ROW = 3
+    ROW_DISTANCE = 2
+    START_CHAR = 6
+    CHAR_DISTANCE = 4
+    x, y = indexes # indexed from 0
+    field_split = field.split("\n")
+    row = field_split[x * ROW_DISTANCE + START_ROW]
+    field_split[x * ROW_DISTANCE + START_ROW] = replace_char_at_index(row, START_CHAR + y * CHAR_DISTANCE, char)
+    return "\n".join(field_split)
+
+
+def get_diagonals_both_directions(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0]) if matrix else 0
+
+    diags = []
+    # Top-left to bottom-right diagonals (↘️)
+    for d in range(rows + cols - 1):
+        diag = []
+        for i in range(max(0, d - cols + 1), min(rows, d + 1)):
+            j = d - i
+            diag.append(matrix[i][j])
+        diags.append(diag)
+
+    # Top-right to bottom-left diagonals (↙️)
+    for d in range(rows + cols - 1):
+        diag = []
+        for i in range(max(0, d - cols + 1), min(rows, d + 1)):
+            j = cols - 1 - (d - i)
+            diag.append(matrix[i][j])
+        diags.append(diag)
+
+    return diags
+
+def is_winning_strike(k, l):
+    i = 0
+    strike_char = None
+    indices_list = []
+    for char, indices in l:
+        if char is None:
+            i = 0
+            strike_char = None
+            indices_list = []
+        else:
+            if strike_char is None:
+                strike_char = char
+                i += 1
+                indices_list.append(indices)
+            else:
+                if char == strike_char:
+                    i += 1
+                    indices_list.append(indices)
+                # When another char spoils strike, its strike must be started
+                else:
+                    i = 1
+                    strike_char = char
+                    indices_list = [indices]
+        if i == k:
+            return strike_char, indices_list
+    return None
+
+
+def get_winner(field_cells, k):
+    columns = [list() for _ in range(len(field_cells))]
+    for row in field_cells:
+        if (char_and_indices := is_winning_strike(k, row)) is not None:
+            return char_and_indices
+        for n, x in enumerate(row):
+            columns[n].append(x)
+    for col in columns:
+        if (char_and_indices := is_winning_strike(k, col)) is not None:
+            return char_and_indices
+
+    for diag in get_diagonals_both_directions(field_cells):
+        if (char_and_indices := is_winning_strike(k, diag)) is not None:
+            return char_and_indices
+    
+    return None
+
+def indices_to_prolong_longest_strike(l):
+    # NOTE: Algorithm will return also potential strikes 
+    # that are limited and can't grow to k-strike 
+    # NOTE: Output lists can be later processed to look for cell with multiple 
+    # possible strikes (fork) to find more optimal moves
+    strike_info_list = []
+    i = 0
+    strike_char = None
+    free_indices_before = None
+    for char, indices in l:
+        # Cell is free
+        if char is None:
+            # No strike has been started yet, but save index of current None 
+            # in case it will start in next cycle
+            if strike_char is None:
+                free_indices_before = indices
+            # Strike was ended, so save its info
+            else:
+                strike_info_list.append((i, strike_char, free_indices_before, indices))
+                free_indices_before = indices # Middle free cell can be also beginning of new potential strike
+            i = 0
+            strike_char = None
+        # Cell is filled
+        else:
+            # First char of potential strike was found
+            if strike_char is None:
+                strike_char = char
+                i += 1
+            else:
+                # Strike is prolonged
+                if char == strike_char:
+                    i += 1
+                # When another char spoils strike, its strike must be started,
+                # and 
+                else:
+                    # Strike can't continue on end, but maybe it can on start,
+                    # so save its info
+                    if free_indices_before is not None:
+                        strike_info_list.append((i, strike_char, free_indices_before, None))
+                    i = 1
+                    strike_char = char
+                    free_indices_before = None # Before is just different char, no free cell
+
+    # Case when strike is on the end of list
+    if i > 0 and free_indices_before is not None:
+        strike_info_list.append((i, strike_char, free_indices_before, None))
+
+    return strike_info_list
+
+def prolong_strike(field_cells, comp_char, player_char, k):
+    potential_strikes_list = []
+    columns = [list() for _ in range(len(field_cells))]
+    for row in field_cells:
+        potential_strikes_list += indices_to_prolong_longest_strike(row)
+        for n, x in enumerate(row):
+            columns[n].append(x)
+    for col in columns:
+        potential_strikes_list += indices_to_prolong_longest_strike(col)
+
+    for diag in get_diagonals_both_directions(field_cells):
+        potential_strikes_list += indices_to_prolong_longest_strike(diag)
+    
+    biggest_comp_tuple, biggest_player_tuple = None, None
+    for i, char, before, after in potential_strikes_list:
+        if char == comp_char:
+            if biggest_comp_tuple is None:
+                biggest_comp_tuple = (i, char, before, after)
+            elif biggest_comp_tuple[0] < i:
+                biggest_comp_tuple = (i, char, before, after)
+        elif char == player_char:
+            if biggest_player_tuple is None:
+                biggest_player_tuple = (i, char, before, after)
+            elif biggest_player_tuple[0] < i:
+                biggest_player_tuple = (i, char, before, after)
+    
+    # Block player only if he has bigger strike than computer,
+    # or on start with only player char on field,
+    # else make computer biggest strike bigger
+    if biggest_comp_tuple is not None:
+        if biggest_player_tuple is not None and biggest_comp_tuple[0] < biggest_player_tuple[0]:
+            return biggest_player_tuple[2] if biggest_player_tuple[2] is not None else biggest_player_tuple[3]
+        return biggest_comp_tuple[2] if biggest_comp_tuple[2] is not None else biggest_comp_tuple[3]
+    if biggest_player_tuple is not None:
+        return biggest_player_tuple[2] if biggest_player_tuple[2] is not None else biggest_player_tuple[3]
+
+
+def player_move(field_cells, char, field, n_int, possible_moves, taken_moves):
+    while True:
+        move = input("Your move (row index followed by column index separated by comma): ")
+        if (m := re.search(r"^(\d+),(\d+)$", move)) is None:
+            continue
+        x, y = m.group(1), m.group(2)
+        x_int, y_int = int(x) - 1, int(y) - 1
+        if x_int < 0 or x_int > n_int - 1 or y_int < 0 or y_int > n_int - 1:
+            print(f"WARNING: Both values must be between 1 and {n_int}")
+            continue
+        if field_cells[x_int][y_int][0] is not None:
+            print("WARNING: Input must be one of empty fields")
+            continue
+        else:
+            field_cells[x_int][y_int][0] = char
+            possible_moves.remove((x_int, y_int))
+            taken_moves.append((x_int, y_int))
+            return put_char_on_field(field, "x", [x_int, y_int])
+            
+def computer_move(possible_moves, char, field_cells, player_char, taken_moves, board_size, k, field = None):
+    # time.sleep(1)
+    # print("Computer move:")
+    if (make_strike := prolong_strike(field_cells, char, player_char, k)):
+        computer_move = make_strike
+    else:
+        computer_move = random.choice(possible_moves)
+    x, y = computer_move
+    field_cells[x][y][0] = char
+    possible_moves.remove(computer_move)
+    taken_moves.append(computer_move)
+    if field is not None:
+        return put_char_on_field(field, "o", computer_move)
+
+def play_tictactoe():
+    k = 3
+    n_int = 5
+    player_round = True
+
+    # k = None
+    # n_int = None
+    # player_round = None
+    # while True:
+    #     input_info = input("Choose board size (1-15), number in row to win (1-15 and smaller or same as board size) and x (goes first) or o, separated by comma: ")
+    #     if (m := re.search(r"^(\d+),(\d+),([x|o])$", input_info)) is None:
+    #         continue
+    #     else:
+    #         n_int = int(m.group(1))
+    #         k = int(m.group(2))
+    #         player_round = True if m.group(3) == "x" else False
+    #         if k < 1 or k > 15 or n_int < 1 or n_int > 15 or k > n_int:
+    #             continue
+    #         break
+    
+    field = create_field_template(n_int)
+    print(field)
+    
+    field_cells = []
+    possible_moves = []
+    taken_moves = []
+    for x in range(n_int):
+        row = []
+        for y in range(n_int):
+            row.append([None, (x, y)])
+            possible_moves.append((x,y))
+        field_cells.append(row)
+
+    player_char, computer_char = ("x", "o") if player_round else ("o", "x")
+    while True:
+        if possible_moves:
+            if player_round:
+                field = player_move(field_cells, player_char, field, n_int, possible_moves, taken_moves)
+                player_round = not player_round
+            else:
+                field = computer_move(possible_moves, computer_char, field_cells, player_char, taken_moves, n_int, k, field)
+                player_round = not player_round
+            if (winner_char_and_indices := get_winner(field_cells, k)) is not None:
+                print(field)
+                print(f"Player {winner_char_and_indices[0]} has {k} strike and is winner!")
+                break
+            print(field)
+        else:
+            print("It's a tie!")
+            break
+
+def generate_field(grid_size):
+    field_cells = []
+    possible_moves = []
+    # taken_moves = []
+    for x in range(grid_size):
+        row = []
+        for y in range(grid_size):
+            row.append([None, (x, y)])
+            possible_moves.append((x,y))
+        field_cells.append(row)
+    return field_cells, possible_moves
+
+# play_tictactoe()
---
+++ main.py
@@ -0,0 +1,253 @@
+"""
+TODO:
+
+DONE:
+- Xs and Os in higher size grids (ie > 20) have uneven margin in cell (small Xs are still off but I don't care anymore)
+- Black border is uneven for some grid sizes, 30 for example. Maybe fix dividing?
+- Bug when I fill first form field and only click on second, start button won't work
+"""
+
+import pygame
+import backend
+import time
+import re
+import asyncio
+
+SCREEN_SIZE = 800
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+clock = pygame.time.Clock()
+
+COLOR_INACTIVE = pygame.Color('black')
+COLOR_ACTIVE = pygame.Color('white')
+FONT = pygame.font.Font(None, 32)
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+        self.remove_default_text = True
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+                if self.remove_default_text:
+                    self.text = ""
+                    self.remove_default_text = False
+                    # Re-render the text.
+                    self.txt_surface = FONT.render(self.text, True, self.color)
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    pass
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(self.rect.w, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+class Button():
+    def __init__(self, x, y, w, h, button_color, text_color, text):
+        self.color = button_color
+        self.rect = pygame.Rect(x, y, w, h)
+        self.text = FONT.render(text, True, text_color)
+
+def draw_x(surface, color, circle_center, circle_radius, thicc):
+    x1 = circle_center[0] - int(circle_radius * 0.707)  # 0.707 is roughly cos(45) or sin(45)
+    y1 = circle_center[1] - int(circle_radius * 0.707)
+    x2 = circle_center[0] + int(circle_radius * 0.707)
+    y2 = circle_center[1] + int(circle_radius * 0.707)
+
+    x3 = circle_center[0] + int(circle_radius * 0.707)
+    y3 = circle_center[1] - int(circle_radius * 0.707)
+    x4 = circle_center[0] - int(circle_radius * 0.707)
+    y4 = circle_center[1] + int(circle_radius * 0.707)
+
+    pygame.draw.line(surface, color, (x1, y1), (x2, y2), thicc)
+    pygame.draw.line(surface, color, (x3, y3), (x4, y4), thicc)
+
+def new_round(winning_indices, field_cells, possible_moves, player_turn, grid_size):
+    winning_indices = []
+    field_cells, possible_moves = backend.generate_field(grid_size)
+    time.sleep(2)
+    player_turn = True
+    return winning_indices, field_cells, possible_moves, player_turn
+
+async def main():
+    grid_size = None
+    k = None
+    button = None
+    input_boxes = []
+    player_char = None
+    computer_char = None
+    player_turn = True
+    winning_indices = []
+    field_cells = possible_moves = None
+    taken_moves = []
+    scene1 = True
+    scene2 = scene3 = False
+    run = True
+    while run:
+        # Events
+        cell_clicked_event = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False 
+            if event.type == pygame.MOUSEBUTTONDOWN:    
+                if event.button == 1:
+                    cell_clicked_event = event
+                if button is not None:
+                    if button.rect.collidepoint(event.pos):
+                        if (m := re.search(r"(\d+)", input_boxes[0].text)) is not None and (n := re.search(r"(\d+)", input_boxes[1].text)) is not None:
+                            grid_size = int(m.group(1))
+                            k = int(n.group(1))
+                            scene2 = False
+                            scene3 = True
+                            cell_clicked_event = None
+                            input_boxes = []
+                            button = None
+                        
+            for box in input_boxes:
+                box.handle_event(event)
+            
+        # Logic
+        for box in input_boxes:
+            box.update()
+        if scene3:
+            cell_size = SCREEN_SIZE // grid_size
+            outer_border = (SCREEN_SIZE % grid_size) // 2
+            if field_cells is None and possible_moves is None:
+                field_cells, possible_moves = backend.generate_field(grid_size)
+            if winning_indices:
+                winning_indices, field_cells, possible_moves, player_turn = new_round(winning_indices, field_cells, possible_moves, player_turn, grid_size)
+            if possible_moves:
+                if player_turn:
+                    if cell_clicked_event is not None:
+                        row = (cell_clicked_event.pos[1] - outer_border) // cell_size
+                        col = (cell_clicked_event.pos[0] - outer_border) // cell_size
+                        if field_cells[row][col][0] is None:
+                            field_cells[row][col][0] = player_char
+                            possible_moves.remove((row, col))
+                            taken_moves.append((row, col))
+                            player_turn = not player_turn
+                else:
+                    backend.computer_move(possible_moves, computer_char, field_cells, player_char, taken_moves, grid_size, k)
+                    player_turn = not player_turn
+                    time.sleep(0.5)
+            else:
+                winning_indices, field_cells, possible_moves, player_turn = new_round(winning_indices, field_cells, possible_moves, player_turn, grid_size)
+            if (winner_char_and_indices := backend.get_winner(field_cells, k)) is not None:
+                winning_indices = winner_char_and_indices[1]
+        
+        # Graphics
+        screen.fill("black")
+        circle_center = SCREEN_SIZE * 3 / 4, SCREEN_SIZE / 2
+        x_center = SCREEN_SIZE / 4, SCREEN_SIZE / 2
+        if scene1:
+            rect1 = pygame.draw.rect(screen, "black", (0, 0, SCREEN_SIZE / 2, SCREEN_SIZE))
+            rect2 = pygame.draw.rect(screen, "black", (SCREEN_SIZE / 2, 0, SCREEN_SIZE / 2, SCREEN_SIZE))
+            radius = 40
+
+            mouse_pos = pygame.mouse.get_pos()
+            if pygame.Rect.collidepoint(rect1, mouse_pos):
+                pygame.draw.rect(screen, "dodgerblue4", (0, 0, SCREEN_SIZE / 2, SCREEN_SIZE))
+                draw_x(screen, "white", x_center, radius, 20)
+                pygame.draw.circle(screen, "white", circle_center, radius, 5)
+            elif pygame.Rect.collidepoint(rect2, mouse_pos):
+                pygame.draw.rect(screen, "firebrick3", (SCREEN_SIZE / 2, 0, SCREEN_SIZE / 2, SCREEN_SIZE))
+                draw_x(screen, "white", x_center, radius, 5)
+                pygame.draw.circle(screen, "white", circle_center, radius, 20)
+
+            if cell_clicked_event is not None:
+                if cell_clicked_event.pos[0] < SCREEN_SIZE / 2:
+                    player_char = "x"
+                    computer_char = "o"
+                else:
+                    player_char = "o"
+                    computer_char = "x"
+                    player_turn = False
+                scene1 = False
+                scene2 = True
+        if scene2:
+            box_width = 50
+            box_height = 32
+            box_gap = 40
+            box_margin = 5
+            center = (SCREEN_SIZE // 2, SCREEN_SIZE * 1 / 4)
+            form_center = (SCREEN_SIZE // 2, SCREEN_SIZE // 2)
+
+            if player_char == "x":
+                pygame.draw.rect(screen, "dodgerblue4", (0, 0, SCREEN_SIZE, SCREEN_SIZE))
+                draw_x(screen, "white", center, radius, 20)
+            elif player_char == "o":
+                pygame.draw.rect(screen, "firebrick3", (0, 0, SCREEN_SIZE, SCREEN_SIZE))
+                pygame.draw.circle(screen, "white", center, radius, 20)
+
+            bs = FONT.render("Board size:", True, "white")
+            sir = FONT.render("Stones in row:", True, "white")
+            screen.blit(bs, (form_center[0] - 5 * box_width, form_center[1] - box_height + box_margin))
+            screen.blit(sir, (form_center[0] - 5 * box_width, form_center[1] - box_height + box_margin + box_gap))
+
+            if not input_boxes: 
+                input_boxes += [
+                    InputBox(form_center[0] - box_width, form_center[1] - box_height, box_width, box_height, "3"),
+                    InputBox(form_center[0] - box_width, form_center[1] - box_height + box_gap, box_width, box_height, "3")
+                ]
+            for box in input_boxes:
+                box.draw(screen)
+            
+            if button is None:
+                button = Button(form_center[0] - box_width, form_center[1] - box_height + 2 * box_gap, 80, 40, "black", "white", "Start")
+
+            pygame.draw.rect(screen, button.color, button.rect)
+            screen.blit(button.text, (button.rect.x + 15, button.rect.y + 10))
+        if scene3:
+            cell_size = SCREEN_SIZE // grid_size
+            outer_border = (SCREEN_SIZE % grid_size) // 2
+            for i, row in enumerate(field_cells):
+                for j, cell in enumerate(row):
+                    rect_left = j * cell_size + 1 + outer_border
+                    rect_top = i * cell_size + 1 + outer_border
+                    border = 2
+
+                    pygame.draw.rect(screen, "white", (rect_left, rect_top, cell_size - border, cell_size - border))
+                    if cell[1] in winning_indices:
+                        pygame.draw.rect(screen, "green", (rect_left, rect_top, cell_size - border, cell_size - border))
+
+                    center = (rect_left + ((cell_size - border) // 2) , rect_top + ((cell_size - border) // 2))
+                    radius = cell_size // 3
+                    thicc = cell_size // 10
+                    if cell[0] == "x":
+                        draw_x(screen, "blue", center, radius, thicc)
+                    elif cell[0] == "o":
+                        pygame.draw.circle(screen, "red", center, radius, thicc)
+        
+        pygame.display.flip()
+        clock.tick(60)
+        await asyncio.sleep(0)
+
+asyncio.run(main())
+pygame.quit()
